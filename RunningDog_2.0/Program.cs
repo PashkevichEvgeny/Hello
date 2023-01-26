e973fe38e4f77b1dafe075a2cb09599e82bed873
PashkevichEvgeny <pash.ev@ya.ru>
2023-01-26 21:51:20 +0800

    add commentary in RunningDog2.0

--- RunningDog_2.0/Program.cs
+++ RunningDog_2.0/Program.cs
@@ -5,11 +5,17 @@ int secondFriendSpeed = 2;
 int dogSpeed = 5;
 int time;
 
-while (distance > 10)
+// Когда растояние становится меньше 10 считаем, что друзья встретились
+// Растояние при встрече должно быть больше 0, иначе цикл станет бесконечным
+while (distance > 10)  
 {
+    // Время, за которое собака добегает до противоположенного друга
     time = distance / (firstFriendSpeed + dogSpeed);
+    // Растояние, которое собака пробегает за это время
     distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    // Когда собака меняет направление, обмениваем скорости друзей
     (firstFriendSpeed, secondFriendSpeed) = (secondFriendSpeed, firstFriendSpeed);
+    // Увеличиваем количество собачьих пробежек
     count++;
 }
 
