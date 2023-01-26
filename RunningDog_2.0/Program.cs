3f586f11bd565dfc7038d8105404e9b567731afb
PashkevichEvgeny <pash.ev@ya.ru>
2023-01-26 20:40:22 +0800

    add new version RunningDog_2.0

---
+++ RunningDog_2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int time;
+
+while (distance > 10)
+{
+    time = distance / (firstFriendSpeed + dogSpeed);
+    distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    (firstFriendSpeed, secondFriendSpeed) = (secondFriendSpeed, firstFriendSpeed);
+    count++;
+}
+
+Console.WriteLine($"Собака пробежала туда-сюда {count} раз");
