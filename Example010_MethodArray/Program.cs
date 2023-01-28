732040bb40053f43a6fe8dc9566c260f64a020d7
PashkevichEvgeny <pash.ev@ya.ru>
2023-01-29 01:10:47 +0800

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти индекс в масcиве для первого совпадения со значением из find
+
+int[] array = { 18, 22, 33, 94, 52, 67, 77, 48, 59, 47, 33, 10 };
+
+int length = array.Length;
+int find = 33;
+int index = 0;
+
+while (index < length)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
