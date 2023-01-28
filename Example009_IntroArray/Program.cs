391aabcac7621f61131c5e7f2bdd0813f4ffe4fb
PashkevichEvgeny <pash.ev@ya.ru>
2023-01-29 00:54:12 +0800

    add Example009_IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if (res < arg2) res = arg2;
+    if (res < arg3) res = arg3;
+    return res;
+}
+// индексы      0   1   2   3   4   5   6   7   8
+int[] array = { 18, 22, 33, 94, 52, 67, 77, 48, 59 };
+
+// Обращение по индексу
+array[0] = 28;
+
+int result = Max(
+    Max(array[0], array[1], array[3]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
